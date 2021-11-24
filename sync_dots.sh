7caefeb430a271ee6b668c239fb05c30dfecba37
2lach <stefanlachmann@hotmail.com>
2021-11-24 20:42:13 +0100

    minor script updates, test sync script and gitignore update

--- sync_dots.sh
+++ sync_dots.sh
@@ -9,14 +9,14 @@ function sync() {
 
 # Folders
 
-declare -a folders=(".config" ".local" ".oh-my-zsh" ".termux" "z-stuff/plugin" "z-stuff/scripts")
+declare -a folders=(".config" ".oh-my-zsh" ".termux" "z-stuff/plugin" "z-stuff/scripts")
 
 for i in "${folders[@]}"; do
   echo "syncing folder: $i"
   sync "$i"
 done
 
-declare -a files=(".gitconfig" ".gitignore" ".hushlogin""z-stuff/books2021.txt" "z-stuff/books_sync.sh" "z-stuff/count-nr-of-read-books.md" "z-stuff/howtoBackupOneplus.txt" "z-stuff/paths.sh" "z-stuff/zsh.local")
+declare -a files=(".gitconfig" ".gitignore" ".hushlogin""z-stuff/books2021.txt" "z-stuff/books_sync.sh" "z-stuff/count-nr-of-read-books.md" "z-stuff/paths.sh" "z-stuff/zsh.local")
 
 for i in "${files[@]}"; do
   echo "sync files: $i"
