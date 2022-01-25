1f57515de5e1490a45af9fde08b1725e4e29afe3
2lach <stefanlachmann@hotmail.com>
2022-01-26 00:19:50 +0100

    update configs

--- .config/packages/list-packages.sh
+++ .config/packages/list-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+mkdir "$(date)"
 pkg list-installed > pkg-installed.txt
-npm-list-globals > npm-packages.txt
+npm list -g --depth=0 > npm-packages.txt
 pip freeze > pip-packages.txt
 gem list --local > gem-packages.txt
