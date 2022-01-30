ef4a419b9f4a735558aa24107863ea6e1617d3ba
2lach <stefanlachmann@hotmail.com>
2022-01-30 12:33:38 +0100

    renamed:    completions/pip-comp.zsh -> completions/pip/pip-comp.zsh 	new file:   scripts/man-colors.sh 	new file:   scripts/test-bash-colors.sh 	modified:   zsh.local

---
+++ scripts/man-colors.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/zsh
+export MANPAGER="less -X --use-color -Dd+r -Du+b"
+
+# export MANPAGER="most"
+
+man() {
+    LESS_TERMCAP_md=$'\e[01;31m' \
+    LESS_TERMCAP_me=$'\e[0m' \
+    LESS_TERMCAP_se=$'\e[0m' \
+    LESS_TERMCAP_so=$'\e[01;44;33m' \
+    LESS_TERMCAP_ue=$'\e[0m' \
+    LESS_TERMCAP_us=$'\e[01;32m' \
+    command man "$@"
+}
