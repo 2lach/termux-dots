e3c7e7b2fbc39c571409d99560c1d48bbc7ad359
2lach <stefanlachmann@hotmail.com>
2022-01-30 22:39:05 +0100

    fix setup step, including setup of packages with various managers

--- .config/packages/list-packages.sh
+++ .config/packages/list-packages.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-mkdir "$(date '+%Y-%m-%d')"
+# mkdir "$(date '+%Y-%m-%d')"
 
-pkg list-installed > pkg-installed.txt
+pkg list-installed > pkg.txt
+sed "s/[/].*//" pkg.txt > pkg-packages.txt
+rm -rf pkg.txt
 npm list -g --depth=0 > npm-packages.txt
 pip freeze > pip-packages.txt
 gem list --local > gem-packages.txt
 
-mv *-packages.txt "$(date '+%Y-%m-%d')"
+# mv *-packages.txt "$(date '+%Y-%m-%d')"
