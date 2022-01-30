cfadf931f9e934fe943269745f07c46bbfd0b32e
2lach <stefanlachmann@hotmail.com>
2022-01-30 22:06:23 +0100

    housekeepin...

--- .config/packages/list-packages.sh
+++ .config/packages/list-packages.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-mkdir "$(date)"
+
+mkdir "$(date '+%Y-%m-%d')"
+
 pkg list-installed > pkg-installed.txt
 npm list -g --depth=0 > npm-packages.txt
 pip freeze > pip-packages.txt
 gem list --local > gem-packages.txt
+
+mv *-packages.txt "$(date '+%Y-%m-%d')"
