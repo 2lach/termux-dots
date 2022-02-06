c82599de2277fee5f57903512c5930229fd4c56d
2lach <stefanlachmann@hotmail.com>
2022-02-06 20:33:06 +0100

    added packages lists and setup script

--- .config/packages/list-packages.sh
+++ .config/packages/list-packages.sh
@@ -2,11 +2,11 @@
 
 # mkdir "$(date '+%Y-%m-%d')"
 
-pkg list-installed > pkg.txt
-sed "s/[/].*//" pkg.txt > pkg-packages.txt
+pkg list-installed >pkg.txt
+sed "s/[/].*//" pkg.txt >pkg-packages.txt
 rm -rf pkg.txt
-npm list -g --depth=0 > npm-packages.txt
-pip freeze > pip-packages.txt
-gem list --local > gem-packages.txt
+npm list -g --depth=0 >npm-packages.txt
+pip freeze >pip-packages.txt
+gem list >gem-packages.txt
 
 # mv *-packages.txt "$(date '+%Y-%m-%d')"
