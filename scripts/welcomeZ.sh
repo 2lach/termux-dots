ffb2fc33b1c2dfc5fd391efbbe46a10c9849cc52
2lach <stefanlachmann@hotmail.com>
2022-09-05 00:18:43 +0200

    new file:   .config/python/.gitignore 	new file:   .config/python/requirement.txt format modified:   scripts/center-terminal-output.sh 	modified:   scripts/convert-mkv-files-mp4.sh 	modified:   scripts/welcomeZ.sh Signed-off-by: 2lach <stefanlachmann@hotmail.com>

--- scripts/welcomeZ.sh
+++ scripts/welcomeZ.sh
@@ -1,3 +1,3 @@
-COLUMNS=$(tput cols) 
+COLUMNS=$(tput cols)
 title=$(date +'%x %T')
-printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title" | lolcat
+printf "%*s\n" $(((${#title} + $COLUMNS) / 2)) "$title" | lolcat
