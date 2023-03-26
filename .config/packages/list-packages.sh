8cd1477ed92362a14c52b1c532837fb6fb8d4c6c
Stefan Lachmann <stefanlachmann@hotmail.com>
2023-03-26 20:49:21 +0200

    update package index Signed-off-by: Stefan Lachmann <stefanlachmann@hotmail.com>

--- .config/packages/list-packages.sh
+++ .config/packages/list-packages.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
 
 # mkdir "$(date '+%Y-%m-%d')"
+
 pkg list-installed | sed "s/[/].*//" >pkg-packages.txt
+
 exa /data/data/com.termux/files/usr/lib/node_modules/ >npm-packages.txt
+
 pip list --format=freeze | grep --color -v '^\-e' | cut -d = -f 1 >pip-packages.txt
+
 gem list >gem-packages.txt
 
 # mv *-packages.txt "$(date '+%Y-%m-%d')"
